71ad867e2ca0b51714e9570b74d4acebaca69dbd
Angai <35431381+naruto6404@users.noreply.github.com>
2018-06-07 01:06:38 +0800

    Add files via upload

---
+++ ReadTest/ReadTest/ReadTest/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace ReadTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.儲存ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(549, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(205, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "開啟圖片";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(12, 52);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox2);
+            this.splitContainer1.Size = new System.Drawing.Size(742, 350);
+            this.splitContainer1.SplitterDistance = 400;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 350);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.ContextMenuStrip = this.contextMenuStrip1;
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(338, 350);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.DoubleClick += new System.EventHandler(this.pictureBox2_DoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.儲存ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(101, 26);
+            // 
+            // 儲存ToolStripMenuItem
+            // 
+            this.儲存ToolStripMenuItem.Name = "儲存ToolStripMenuItem";
+            this.儲存ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.儲存ToolStripMenuItem.Text = "儲存";
+            this.儲存ToolStripMenuItem.Click += new System.EventHandler(this.儲存ToolStripMenuItem_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(35, 21);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(59, 16);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "均值化";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(112, 21);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(71, 16);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "三維映射";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "RGB",
+            "RBG",
+            "GRB",
+            "GBR",
+            "BRG",
+            "BGR"});
+            this.comboBox1.Location = new System.Drawing.Point(189, 17);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.Text = "RGB";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(766, 414);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResizeBegin += new System.EventHandler(this.Form1_Resize);
+            this.ResizeEnd += new System.EventHandler(this.Form1_Resize);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 儲存ToolStripMenuItem;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ ReadTest/ReadTest/ReadTest/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReadTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            go();
+        }
+
+        public void go()
+        {
+            if (System.IO.File.Exists(openFileDialog1.FileName))
+            {
+                pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+                Bitmap b = (Bitmap)pictureBox1.Image;
+                Bitmap b128 = new Bitmap(b);
+                Bitmap bb = new Bitmap(b);
+
+                int len​​gth = b.Height * 3 * b.Width;
+                byte[] RGB = new byte[length];
+                byte[] binary128 = new byte[length];
+                BitmapData data = b.LockBits(new Rectangle(0, 0, b.Width, b.Height), System.Drawing.Imaging.ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+                BitmapData data_b128 = b128.LockBits(new Rectangle(0, 0, b.Width, b.Height), System.Drawing.Imaging.ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+                System.IntPtr Scan0 = data.Scan0;
+                System.IntPtr Scan0_b128 = data_b128.Scan0;
+                System.Runtime.InteropServices.Marshal.Copy(Scan0, RGB, 0, length);
+                System.Runtime.InteropServices.Marshal.Copy(Scan0_b128, binary128, 0, length);
+                double gray = 0;
+
+                int[] cc = new int[5];
+
+                int[] C = get_Color();
+
+                for (int i = 0; i < RGB.Length; i += 3)
+                {
+                    //灰階
+                    if (radioButton1.Checked)
+                        gray = (RGB[i] + RGB[i + 1] + RGB[i + 2]) / 3;
+                    else if (radioButton2.Checked){
+                        gray = RGB[ i + C[0]] * 0.2990 + RGB[i + C[1]] * 0.58700 + RGB[i + C[2]] * 0.11400;
+                    }
+                        
+
+                    RGB[i + 2] = RGB[i + 1] = RGB[i] = (byte)gray;
+
+                    //二值化
+                    if (RGB[i] > 127)
+                        binary128[i + 2] = binary128[i + 1] = binary128[i] = 255;
+                    else
+                        binary128[i + 2] = binary128[i + 1] = binary128[i] = 0;
+
+                    //五值化灰階
+                    if (RGB[i] >= 204)
+                        RGB[i + 2] = RGB[i + 1] = RGB[i] = 255;
+                    else if (RGB[i] >= 153 && RGB[i] < 204)
+                        RGB[i + 2] = RGB[i + 1] = RGB[i] = 191;
+                    else if (RGB[i] >= 102 && RGB[i] < 153)
+                        RGB[i + 2] = RGB[i + 1] = RGB[i] = 127;
+                    else if (RGB[i] >= 51 && RGB[i] < 102)
+                        RGB[i + 2] = RGB[i + 1] = RGB[i] = 63;
+                    else
+                        RGB[i + 2] = RGB[i + 1] = RGB[i] = 0;
+
+                    if (RGB[i] == 255)
+                        cc[4]++;
+                    else if (RGB[i] == 191)
+                        cc[3]++;
+                    else if (RGB[i] == 127)
+                        cc[2]++;
+                    else if (RGB[i] == 63)
+                        cc[1]++;
+                    else
+                        cc[0]++;
+                }
+
+                int[] coo = { 0, 63, 127, 191, 255 };
+                int co = 0;
+                for (int i = 0; i < cc.Length; i++)
+                {
+                    if (i > 0 && cc[i] > cc[i - 1])
+                        co = coo[i];
+                    if (i == 0)
+                        co = coo[0];
+                }
+
+                for (int i = 0; i < RGB.Length; i += 3)
+                {
+                    if (co > 64)
+                    {
+                        //二值化
+                        if (RGB[i] >= co)
+                            RGB[i + 2] = RGB[i + 1] = RGB[i] = 255;
+                        else
+                            RGB[i + 2] = RGB[i + 1] = RGB[i] = 0;
+                    }
+                    if (RGB[i] >= 50)
+                        RGB[i + 2] = RGB[i + 1] = RGB[i] = 255;
+                    else
+                        RGB[i + 2] = RGB[i + 1] = RGB[i] = 0;
+
+                    if (RGB[i] != binary128[i])
+                        RGB[i + 2] = RGB[i + 1] = RGB[i] = (byte)((RGB[i] + binary128[i]) / 2);
+                }
+                System.Runtime.InteropServices.Marshal.Copy(RGB, 0, Scan0, length);
+                System.Runtime.InteropServices.Marshal.Copy(binary128, 0, Scan0_b128, length);
+                b.UnlockBits(data);
+                b128.UnlockBits(data_b128);
+                pictureBox1.Image = bb;
+                pictureBox2.Image = b;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            splitContainer1.SplitterDistance = splitContainer1.Width / 2;
+        }
+
+        private void pictureBox2_DoubleClick(object sender, EventArgs e)
+        {
+            if (pictureBox2.Image != null)
+            {
+                System.Windows.Forms.Clipboard.SetImage(pictureBox2.Image);
+                MessageBox.Show("已複製到剪貼簿");
+            }
+        }
+        private void 儲存ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (pictureBox2.Image != null)
+            {
+                System.Windows.Forms.Clipboard.SetImage(pictureBox2.Image);
+                MessageBox.Show("已複製到剪貼簿");
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            comboBox1.Enabled = radioButton2.Checked;
+            go();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            go();
+        }
+
+        public int[] get_Color()
+        {
+            int[] temp = new int[3];
+            int i = 0;
+            switch (comboBox1.Text)
+            {
+                case "RGB":
+                    temp = new int[] { i + 0, i + 1, i + 2 };
+                    break;
+                case "RBG":
+                    temp = new int[] { i + 0, i + 2, i + 1 };
+                    break;
+                case "GRB":
+                    temp = new int[] { i + 1, i + 0, i + 2 };
+                    break;
+                case "GBR":
+                    temp = new int[] { i + 1, i + 2, i + 0 };
+                    break;
+                case "BRG":
+                    temp = new int[] { i + 2, i + 0, i + 1 };
+                    break;
+                case "BGR":
+                    temp = new int[] { i + 2, i + 1, i + 0 };
+                    break;
+            }
+            return temp;
+        }
+    }
+}
